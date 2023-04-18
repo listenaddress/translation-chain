57f12aba0ad665c0c06d909a0f30225e1749dc57
Thomas O'Brien <tbobrien612@gmail.com>
2023-04-18 14:48:58 -0500

    Add all steps

--- common/translation_chain.py
+++ common/translation_chain.py
@@ -17,6 +17,7 @@ class Step:
 class TranslationChain():
     created_at: float = field(default_factory=time.time)
     last_updated_at: float = field(default_factory=time.time)
+    finished_at: float = None
     message: str = ""
     target_domain: str = ""
     hash: str = ""
@@ -55,11 +56,12 @@ class TranslationChain():
 
             if not step.finished:
                 try:
-                  print("Starting ", step.type)
                   step.output = getattr(self, step.type)()
                   step.finished = True
                   self.steps[self.current_step] = asdict(step)
                   object.__setattr__(self, "current_step", self.current_step + 1)
+                  if self.current_step == len(self.steps):
+                    object.__setattr__(self, "finished_at", time.time())
                   self.save()
                   print("Finished ", step.type)
                 except Exception as e:
@@ -67,11 +69,60 @@ class TranslationChain():
                   print(e)
                   break
         
-        print("Finished running translation chain")
+        
     
     def get_neuroscience_concepts_and_possible_translations(self):
       return "They talk about the hippocampus and the amygdala. Theses could be translated to body and person." 
 
+    def find_relevant_papers(self):
+      return [
+        {
+          "title": "The hippocampus and the amygdala are important for memory",
+          "abstract": "The hippocampus and the amygdala are important for memory",
+          "url": "https://www.semanticscholar.org/paper/The-hippocampus-and-the-amygdala-are-important-for-Blair/7b9f1c8f7f8b0d7e7a2c2e8d1b0c2b4e0b4a9c8d"
+        },
+        {
+          "title": "The hippocampus and the amygdala are important for memory",
+          "abstract": "The hippocampus and the amygdala are important for memory",
+          "url": "https://www.semanticscholar.org/paper/The-hippocampus-and-the-amygdala-are-important-for-Blair/7b9f1c8f7f8b0d7e7a2c2e8d1b0c2b4e0b4a9c8d"
+        }
+      ]
+    
+    def summarize_relevance_of_papers(self):
+      return "The hippocampus and the amygdala are important for memory"
+    
+    def summarize_best_translation_options(self):
+      return "The best options for translating the hippocampus and the amygdala are body and person"
+    
+    def translate(self):
+      return "The body and the person are important for memory"
+    
+    def find_relevant_papers_for_translation(self):
+        return [
+            {
+                "title": "Memory and the hippocampus: a synthesis from findings with rats, monkeys, and humans",
+                "abstract": "The hippocampus and the amygdala are important for memory",
+                "url": "https://www.semanticscholar.org/paper/The-hippocampus-and-the-amygdala-are-important-for-Blair/7b9f1c8f7f8b0d7e7a2c2e8d1b0c2b4e0b4a9c8d"
+            },
+            {
+                "title": "When memory fails: the role of the hippocampus in amnesia",
+                "abstract": "The hippocampus and the amygdala are important for memory",
+                "url": "https://www.semanticscholar.org/paper/The-hippocampus-and-the-amygdala-are-important-for-Blair/7b9f1c8f7f8b0d7e7a2c2e8d1b0c2b4e0b4a9c8d"
+            }
+        ]
+    
+    def summarize_relevance_of_papers_for_translation(self):
+        return "The hippocampus and the amygdala are important for memory"
+    
+    def adjust_abstract(self):
+        return "The body and the person are important for memory"
+    
+    def critique(self):
+        return "After reviewing the translation, I think it is good to go"
+    
+    def get_final_translation(self):
+        object.__setattr__(self, "final_translation", "The body and the person are important for memory")
+        return "Final translation: The body and the person are important for memory"
 
     @classmethod
     def load(cls, message=message, hash=hash):
@@ -104,17 +155,13 @@ class TranslationChain():
 def get_default_steps():
     return [
         Step(type="get_neuroscience_concepts_and_possible_translations"),
-        # Step(type="find_relevant_papers"),
-        # Step(type="summarize_relevance_of_papers"),
-        # Step(type="summarize_best_translation_options"),
-        # Step(type="translate"),
-        # Step(type="find_relevant_papers_for_translation"),
-        # Step(type="summarize_relevance_of_papers_for_translation"),
-        # Step(type="adjust_abstract"),
-        # Step(type="critique"),
-        # Step(type="add_citations"),
-        # Step(type="save_translation"),
-        # Step(type="review_translation"),
+        Step(type="find_relevant_papers"),
+        Step(type="summarize_relevance_of_papers"),
+        Step(type="summarize_best_translation_options"),
+        Step(type="translate"),
+        Step(type="find_relevant_papers_for_translation"),
+        Step(type="summarize_relevance_of_papers_for_translation"),
+        Step(type="adjust_abstract"),
+        Step(type="critique"),
+        Step(type="get_final_translation")
     ]
-
-
--- scripts/translate.py
+++ scripts/translate.py
@@ -1,5 +1,13 @@
 import sys
-from common.translation_chain import TranslationChain
+from common.translation_chain import TranslationChain, Step
 
 chain = TranslationChain.load(sys.argv[1])
 chain.run()
+
+# Print out the final translation and how long it took to translate
+if chain.finished_at:
+    print("Translation: " + chain.final_translation)
+    print("Time to translate: " + str(chain.finished_at - chain.created_at) + " seconds")
+else:
+    print("Translation not finished.")
+    print("Last step finished: " + Step(**chain.steps[chain.current_step - 1]).type)
